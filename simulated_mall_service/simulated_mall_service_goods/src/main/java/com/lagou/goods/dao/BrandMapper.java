40eab2e31ef57cefc496cd37a76ec91f897147c0
Siwei Xing <luxness.int@gmail.com>
2023-08-22 20:00:13 -0700

    mall's skeleton

---
+++ simulated_mall_service/simulated_mall_service_goods/src/main/java/com/lagou/goods/dao/BrandMapper.java
@@ -0,0 +1,8 @@
+package com.lagou.goods.dao;
+
+import com.lagou.goods.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BrandMapper extends Mapper<Brand> {
+
+}
---
+++ simulated_mall_service/simulated_mall_service_goods/src/main/java/com/lagou/goods/dao/SpecMapper.java
@@ -0,0 +1,8 @@
+package com.lagou.goods.dao;
+
+import com.lagou.goods.pojo.Spec;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SpecMapper extends Mapper<Spec> {
+
+}
